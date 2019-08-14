c04ce23ee2ba525a640b74d38cc1930810d973b4
Baron Hartono <baronhartono@gmail.com>
2019-08-14 11:17:33 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import ManagePosts from './components/ManagePosts';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ManagePosts />
+        <Header navBrand={'Instagrin'} />
+        <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/register" component={Register} />
+            <Route path="/waitingverification" component={WaitingVerification} />
+            <Route path="/verified" component={Verified} />
+          </div>
       </div>
     )
   }
---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,26 @@
+import { 
+    USER_LOGIN_SUCCESS, 
+    AUTH_SYSTEM_ERROR, 
+    AUTH_LOADING
+} from '../actions/types';
+
+const INITIAL_STATE = { 
+    username: '', 
+    email: '', 
+    status: '',  
+    error: '', 
+    loading: false 
+};
+
+export default (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        case USER_LOGIN_SUCCESS :
+            return { ...INITIAL_STATE, ...action.payload };
+        case AUTH_SYSTEM_ERROR :
+            return { ...INITIAL_STATE, error: action.payload }
+        case AUTH_LOADING :
+            return { ...state, loading: true }
+        default :
+            return state;
+    }
+}
