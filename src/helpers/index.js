610a782d0b18f5b93a3765091ed9d5d2917913ab
Baron Hartono <baronhartono@gmail.com>
2019-08-16 14:49:50 +0700

    commit

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:1997';
-// export const API_URL = 'https://api-image-baron.herokuapp.com';
+// export const API_URL = 'http://localhost:1997';
+export const API_URL = 'https://api-image-baron.herokuapp.com';
