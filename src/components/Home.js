66f7d274440e2d3de07584871909afd0241623ce
Baron Hartono <baronhartono@gmail.com>
2019-08-16 14:49:08 +0700

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Test from './Test';
 
 class Home extends Component {
     render() {
         return (
             <div>
                 <h1>Ini Home</h1>
+                <Test />
             </div>
         );
     }
